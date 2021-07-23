dbdd7d52c70df5cd017d126bf8036d55400ed59b
parvinfrzd <parv93farah@gmail.com>
2021-07-23 15:10:16 -0400

    add webpack project

--- webpack/src/script.js
+++ webpack/src/script.js
@@ -1,1 +1,28 @@
 import './style.css'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+const position = {
+    x: 0,
+    y: 1,
+    z: 3
+};
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(position.x, position.y, position.z)
+scene.add(camera);
+
+const canvas = document.querySelector('canvas.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
